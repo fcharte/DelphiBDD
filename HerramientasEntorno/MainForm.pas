3fb0b05aff7a440dc44065fa56461e18f9b157a7
fcharte <francisco@fcharte.com>
2015-02-05 13:00:24 +0100

    Añadidas conexiones mediante drag&drop

--- HerramientasEntorno/MainForm.pas
+++ HerramientasEntorno/MainForm.pas
@@ -4,10 +4,20 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.Phys.SQLite, FireDAC.Phys.SQLiteDef,
+  FireDAC.Stan.ExprFuncs, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
+  FireDAC.DApt, FireDAC.Phys.MSAcc, FireDAC.Phys.MSAccDef, Data.DB,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
   TForm3 = class(TForm)
+    HolafiredacConnection: TFDConnection;
+    EntradasTable: TFDQuery;
+    DbdemosConnection: TFDConnection;
+    CustomerTable: TFDQuery;
   private
     { Private declarations }
   public
