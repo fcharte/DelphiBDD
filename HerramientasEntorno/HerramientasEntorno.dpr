29165d2fd28151e453d2f656969df6702e4091fa
fcharte <francisco@fcharte.com>
2015-02-05 17:35:31 +0100

    Agregado data module

--- HerramientasEntorno/HerramientasEntorno.dpr
+++ HerramientasEntorno/HerramientasEntorno.dpr
@@ -3,12 +3,14 @@ program HerramientasEntorno;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  MainForm in 'MainForm.pas' {Form3};
+  MainForm in 'MainForm.pas' {Form3},
+  DataModule in 'DataModule.pas' {DataModule2: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm3, Form3);
+  Application.CreateForm(TDataModule2, DataModule2);
   Application.Run;
 end.
