a11e6cbf3eb1faa2bd75aadadb5b5f79ec9b52c4
fcharte <francisco@fcharte.com>
2015-02-10 19:11:50 +0100

    Agregada conexión a tabla CUSTOMER

--- GUIconFMX/DataModule.dfm
+++ GUIconFMX/DataModule.dfm
@@ -2,4 +2,23 @@ object DataModule2: TDataModule2
   OldCreateOrder = False
   Height = 259
   Width = 383
+  object DbdemosConnection: TFDConnection
+    Params.Strings = (
+      'ConnectionDef=DBDEMOS')
+    LoginPrompt = False
+    Left = 67
+    Top = 31
+  end
+  object CustomerTable: TFDQuery
+    Connection = DbdemosConnection
+    SQL.Strings = (
+      'SELECT * FROM customer')
+    Left = 67
+    Top = 79
+  end
+  object FDGUIxWaitCursor1: TFDGUIxWaitCursor
+    Provider = 'FMX'
+    Left = 240
+    Top = 32
+  end
 end
--- GUIconFMX/DataModule.pas
+++ GUIconFMX/DataModule.pas
@@ -3,10 +3,18 @@ unit DataModule;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MSAcc,
+  FireDAC.Phys.MSAccDef, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
+  FireDAC.DApt, FireDAC.FMXUI.Wait, FireDAC.Comp.UI, Data.DB,
+  FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
   TDataModule2 = class(TDataModule)
+    DbdemosConnection: TFDConnection;
+    CustomerTable: TFDQuery;
+    FDGUIxWaitCursor1: TFDGUIxWaitCursor;
   private
     { Private declarations }
   public
