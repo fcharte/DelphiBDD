4b228bd5c6c1171af2d83c0366e9744ea6082b85
fcharte <francisco@fcharte.com>
2015-02-10 19:11:09 +0100

    Añadido módulo de datos

---
+++ GUIconFMX/DataModule.dfm
@@ -0,0 +1,5 @@
+object DataModule2: TDataModule2
+  OldCreateOrder = False
+  Height = 259
+  Width = 383
+end
--- GUIconFMX/GUIconFMX.dpr
+++ GUIconFMX/GUIconFMX.dpr
@@ -3,12 +3,14 @@ program GUIconFMX;
 uses
   System.StartUpCopy,
   FMX.Forms,
-  MainForm in 'MainForm.pas' {Form4};
+  MainForm in 'MainForm.pas' {Form4},
+  DataModule in 'DataModule.pas' {DataModule2: TDataModule};
 
 {$R *.res}
 
 begin
   Application.Initialize;
   Application.CreateForm(TForm4, Form4);
+  Application.CreateForm(TDataModule2, DataModule2);
   Application.Run;
 end.
