1bbf436aeedab3f6a41a2fefbb53475331063236
fcharte <francisco@fcharte.com>
2015-02-10 18:33:39 +0100

    Agregada conexión a tabla CUSTOMER

--- GUIconVCL/DataModule.dfm
+++ GUIconVCL/DataModule.dfm
@@ -2,4 +2,18 @@ object DataModule2: TDataModule2
   OldCreateOrder = False
   Height = 150
   Width = 215
+  object DbdemosConnection: TFDConnection
+    Params.Strings = (
+      'ConnectionDef=DBDEMOS')
+    LoginPrompt = False
+    Left = 48
+    Top = 40
+  end
+  object CustomerTable: TFDQuery
+    Connection = DbdemosConnection
+    SQL.Strings = (
+      'SELECT * FROM customer')
+    Left = 48
+    Top = 88
+  end
 end
--- GUIconVCL/DataModule.pas
+++ GUIconVCL/DataModule.pas
@@ -3,10 +3,16 @@ unit DataModule;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MSAcc,
+  FireDAC.Phys.MSAccDef, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
+  FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
   TDataModule2 = class(TDataModule)
+    DbdemosConnection: TFDConnection;
+    CustomerTable: TFDQuery;
   private
     { Private declarations }
   public
