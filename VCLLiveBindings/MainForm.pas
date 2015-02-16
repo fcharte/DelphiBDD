2da32e95f34325e3e05c707dc924b8cb94e4f828
fcharte <francisco@fcharte.com>
2015-02-16 17:25:05 +0100

    Interfaz con LiveBindings

--- VCLLiveBindings/MainForm.pas
+++ VCLLiveBindings/MainForm.pas
@@ -4,10 +4,26 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Data.Bind.GenData, System.Rtti,
+  System.Bindings.Outputs, Vcl.Bind.Editors, Data.Bind.EngExt,
+  Vcl.Bind.DBEngExt, Vcl.StdCtrls, Vcl.ExtCtrls, Vcl.Mask, Data.Bind.Components,
+  Data.Bind.ObjectScope;
 
 type
   TForm5 = class(TForm)
+    PrototypeBindSource1: TPrototypeBindSource;
+    BindingsList1: TBindingsList;
+    LabeledEditContactName: TLabeledEdit;
+    LinkControlToFieldContactName12: TLinkControlToField;
+    LabeledEditContactTitle1: TLabeledEdit;
+    LinkControlToFieldContactTitle1: TLinkControlToField;
+    MaskEditCurrencyField1: TMaskEdit;
+    LinkControlToFieldCurrencyField1: TLinkControlToField;
+    Label1: TLabel;
+    ImageContactBitmap1: TImage;
+    LinkControlToFieldContactBitmap1: TLinkControlToField;
+    MemoStringsField1: TMemo;
+    LinkControlToFieldStringsField1: TLinkControlToField;
   private
     { Private declarations }
   public
