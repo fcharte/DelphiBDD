2ec9e8675b2d01af6e104d439f2f963acf0224e3
fcharte <francisco@fcharte.com>
2015-02-20 08:39:31 +0100

    Nuevo proyecto IBEmbebido

---
+++ IBEmbebido/IBEmbebido.dpr
@@ -0,0 +1,14 @@
+program IBEmbebido;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  MainForm in 'MainForm.pas' {frmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TfrmMain, frmMain);
+  Application.Run;
+end.
