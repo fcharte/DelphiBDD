938f0fdd70ae2f327b43b6dc6169d0eb2048641e
fcharte <francisco@fcharte.com>
2015-02-23 17:29:49 +0100

    Configuración básica de datos

--- BDDMemoria/DataModule.dfm
+++ BDDMemoria/DataModule.dfm
@@ -2,4 +2,24 @@ object dmMemoria: TdmMemoria
   OldCreateOrder = False
   Height = 317
   Width = 517
+  object FDConnection1: TFDConnection
+    Params.Strings = (
+      'DriverID=SQLite')
+    LoginPrompt = False
+    Left = 56
+    Top = 40
+  end
+  object FDLocalSQL1: TFDLocalSQL
+    Left = 56
+    Top = 112
+  end
+  object FDGUIxWaitCursor1: TFDGUIxWaitCursor
+    Provider = 'FMX'
+    Left = 416
+    Top = 48
+  end
+  object FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink
+    Left = 424
+    Top = 104
+  end
 end
--- BDDMemoria/DataModule.pas
+++ BDDMemoria/DataModule.pas
@@ -3,10 +3,18 @@ unit DataModule;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.SQLite,
+  FireDAC.Phys.SQLiteDef, FireDAC.Stan.ExprFuncs, FireDAC.FMXUI.Wait,
+  FireDAC.Comp.UI, FireDAC.Comp.Client, FireDAC.Phys.SQLiteVDataSet, Data.DB;
 
 type
   TdmMemoria = class(TDataModule)
+    FDConnection1: TFDConnection;
+    FDLocalSQL1: TFDLocalSQL;
+    FDGUIxWaitCursor1: TFDGUIxWaitCursor;
+    FDPhysSQLiteDriverLink1: TFDPhysSQLiteDriverLink;
   private
     { Private declarations }
   public
