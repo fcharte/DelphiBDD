24c3ea5d19e7726ee917826cb4eebd4ba17972c4
Francisco Charte Ojeda <francisco@fcharte.com>
2015-03-20 13:59:42 +0100

    Lectura CSV con codificación UTF-8

--- UnicodeBDD/MainForm.pas
+++ UnicodeBDD/MainForm.pas
@@ -4,10 +4,13 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.Memo;
 
 type
   TForm6 = class(TForm)
+    Memo1: TMemo;
+    procedure FormCreate(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +23,10 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TForm6.FormCreate(Sender: TObject);
+begin
+  Memo1.Lines.LoadFromFile('Vocab.csv', TEncoding.UTF8);
+end;
 
 end.
