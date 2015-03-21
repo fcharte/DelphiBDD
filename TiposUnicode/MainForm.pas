e195453d0531afe0b04d487d3cab6c3875c79d62
Francisco Charte Ojeda <francisco@fcharte.com>
2015-03-21 09:52:11 +0100

    GUI para probar con cadenas Unicode/Ansi

--- TiposUnicode/MainForm.pas
+++ TiposUnicode/MainForm.pas
@@ -4,10 +4,23 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FMX.Controls.Presentation, FMX.Edit, FMX.StdCtrls;
 
 type
   TForm1 = class(TForm)
+    Label1: TLabel;
+    Label2: TLabel;
+    edUnicodeString: TEdit;
+    edAnsiString: TEdit;
+    Label3: TLabel;
+    Label4: TLabel;
+    Label5: TLabel;
+    Label6: TLabel;
+    longUnicode: TEdit;
+    longAnsi: TEdit;
+    bytesUnicode: TEdit;
+    bytesString: TEdit;
   private
     { Private declarations }
   public
