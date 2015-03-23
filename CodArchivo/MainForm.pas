bf439c5b2814afd560838ad702fe70dd6ee01c89
Francisco Charte Ojeda <francisco@fcharte.com>
2015-03-23 19:49:39 +0100

    GUI

--- CodArchivo/MainForm.pas
+++ CodArchivo/MainForm.pas
@@ -4,10 +4,20 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Layouts, FMX.Memo;
 
 type
   TfrmMain = class(TForm)
+    OpenDialog1: TOpenDialog;
+    ToolBar1: TToolBar;
+    btnAbrir: TButton;
+    btnGuardar: TButton;
+    Memo1: TMemo;
+    UTF16: TRadioButton;
+    UTF8: TRadioButton;
+    ANSI: TRadioButton;
+    BOM: TCheckBox;
   private
     { Private declarations }
   public
