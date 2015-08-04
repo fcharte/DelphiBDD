a34bb6a6a7ba71cc11e293ccfd74165f32927b5d
Francisco Charte Ojeda <francisco@fcharte.com>
2015-08-04 11:25:06 +0200

    Lista de conexiones y parámetros

--- CSConnection/MainForm.pas
+++ CSConnection/MainForm.pas
@@ -4,10 +4,15 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.Layouts,
+  FMX.ListBox;
 
 type
   TfrmMain = class(TForm)
+    ListBox1: TListBox;
+    ListBox2: TListBox;
+    procedure FormCreate(Sender: TObject);
+    procedure ListBox1Change(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +25,17 @@ var
 implementation
 
 {$R *.fmx}
+
+uses DataModule, FireDAC.Comp.Client;
+
+procedure TfrmMain.FormCreate(Sender: TObject);
+begin
+  FDManager.GetConnectionDefNames(ListBox1.Items);
+end;
+
+procedure TfrmMain.ListBox1Change(Sender: TObject);
+begin
+  FDManager.GetConnectionDefParams(ListBox1.Selected.Text, ListBox2.Items);
+end;
 
 end.
