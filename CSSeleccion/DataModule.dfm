d5cd033bdbc4640e1c45f71527d4e9fb2fdcc58f
Francisco Charte Ojeda <francisco@fcharte.com>
2015-08-05 10:02:21 +0200

    Cambiada tabla por consulta

--- CSSeleccion/DataModule.dfm
+++ CSSeleccion/DataModule.dfm
@@ -1,5 +1,21 @@
 object dmSeleccion: TdmSeleccion
   OldCreateOrder = False
-  Height = 150
-  Width = 215
+  Height = 224
+  Width = 415
+  object AdventureworksConnection: TFDConnection
+    Params.Strings = (
+      'ConnectionDef=AdventureWorks')
+    Connected = True
+    LoginPrompt = False
+    Left = 184
+    Top = 54
+  end
+  object ProductCategoryQuery: TFDQuery
+    Connection = AdventureworksConnection
+    SQL.Strings = (
+      'SELECT ProductCategoryID, Name'
+      'FROM Production.ProductCategory')
+    Left = 184
+    Top = 128
+  end
 end
--- CSSeleccion/DataModule.pas
+++ CSSeleccion/DataModule.pas
@@ -3,10 +3,16 @@ unit DataModule;
 interface
 
 uses
-  System.SysUtils, System.Classes;
+  System.SysUtils, System.Classes, FireDAC.Stan.Intf, FireDAC.Stan.Option,
+  FireDAC.Stan.Error, FireDAC.UI.Intf, FireDAC.Phys.Intf, FireDAC.Stan.Def,
+  FireDAC.Stan.Pool, FireDAC.Stan.Async, FireDAC.Phys, FireDAC.Phys.MSSQL,
+  FireDAC.Phys.MSSQLDef, FireDAC.Stan.Param, FireDAC.DatS, FireDAC.DApt.Intf,
+  FireDAC.DApt, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client;
 
 type
   TdmSeleccion = class(TDataModule)
+    AdventureworksConnection: TFDConnection;
+    ProductCategoryQuery: TFDQuery;
   private
     { Private declarations }
   public
