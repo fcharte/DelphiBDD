e38488a8df2a1fdc891c923119bf6438e9733b18
Francisco Charte Ojeda <francisco@fcharte.com>
2015-08-26 09:35:55 +0200

    Gestión seguridad

--- CSInterBase/MainForm.pas
+++ CSInterBase/MainForm.pas
@@ -8,12 +8,13 @@ uses
   FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
   FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
   FireDAC.Phys, FireDAC.Phys.IB, FireDAC.Phys.IBDef, FireDAC.Phys.IBBase,
-  Data.DB, FireDAC.Comp.Client;
+  Data.DB, FireDAC.Comp.Client, FireDAC.Phys.IBWrapper;
 
 type
   TfrmMain = class(TForm)
     FDConnection1: TFDConnection;
     FDPhysIBDriverLink1: TFDPhysIBDriverLink;
+    FDIBSecurity1: TFDIBSecurity;
   private
     { Private declarations }
   public
