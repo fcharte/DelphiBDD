be5fbb98b27982e18e3ed563d72814c1065d22ec
Francisco Charte Ojeda <francisco@fcharte.com>
2015-08-30 13:49:36 +0200

    Enumeración con valores

--- EnumeratedType/EnumeratedType.dpr
+++ EnumeratedType/EnumeratedType.dpr
@@ -10,10 +10,14 @@ uses
 type
   TProductCategory = (Snacks, Crunchies, Smoothies);
 
+  TSnacksCategory = (
+     Almonds = 5, Crackers = 8, Muffins = 10);
+
 var
   aProductCategory: TProductCategory;
-
+  aSnack: TSnacksCategory;
 begin
 
   aProductCategory := TProductCategory.Snacks;
+  aSnack := TSnacksCategory.Almonds;
 end.
