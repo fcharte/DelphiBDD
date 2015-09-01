3e6a1c1842e5ccbe69fa33fed4c124b7c607162b
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-01 10:42:19 +0200

    Fin proyecto AnonymousMethods

--- AnonymousMethods/AnonymousMethods.dpr
+++ AnonymousMethods/AnonymousMethods.dpr
@@ -1,36 +1,48 @@
 program AnonymousMethods;
 
 {$APPTYPE CONSOLE}
-
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils, System.Types, System.StrUtils;
 
-procedure Sort(items: TStringDynArray;
-                 compareFunc: TFuncComparator);
+type
 
+  TFuncComparator = reference to function(l1, l2: String): Boolean;
+
+procedure Sort(items: TStringDynArray; compareFunc: TFuncComparator);
 var
   index, position: Integer;
   temp: String;
 begin
-  for index := Low(items)+1 to High(items) do
+  for index := Low(items) + 1 to High(items) do
+  begin
+    temp := items[index];
+    position := index - 1;
+
+    while ((position >= Low(items)) and compareFunc(temp, items[position])) do
     begin
-      temp := items[index];
-      position := index – 1;
-
-      while ((position >= Low(items)) and
-              compareFunc(temp, items[position])) do
-        begin
-          items[position+1] := items[position];
-          dec(position);
-        end;
-
-       items[position+1] := temp;
+      items[position + 1] := items[position];
+      dec(position);
     end;
+
+    items[position + 1] := temp;
+  end;
 end;
 
 
-begin
+var
+  months: TStringDynArray;
 
+begin
+  months := SplitString('Ene,Feb,Mar,Abr,May,Jun,' +
+                        'Jul,Ago,Sep,Oct,Nov,Dic', ',');
+
+  Sort(months, function(l1, l2: String): Boolean
+    begin
+      if l1 < l2 then
+        result := true
+      else
+        result := false;
+    end);
 end.
