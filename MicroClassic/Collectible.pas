ef337034c68ebcaffc69acf7ae64e7e92029ca0b
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-01 09:39:36 +0200

    Definición de propiedades

--- MicroClassic/Collectible.pas
+++ MicroClassic/Collectible.pas
@@ -25,6 +25,15 @@ type
     { Sólo hay un constructor.
       Necesita el nombre del objeto a registrar }
     constructor Create(name: String);
+    property Name: String read Fname write setName;
+    { Acceso a los demás datos contenidos en el objeto }
+    property Year: TLaunchYear read Fyear write Fyear;
+    property State: TConsState read Fstate write Fstate;
+    property Description: String read Fdescription write Fdescription;
+    property NumPictures: Integer read nPictures;
+    procedure addPicture(picture: TBitmap);
+    function hasPictures: Boolean;
+property Picture[I: Integer] : TBitmap read getPicture;
   end;
 
 implementation
