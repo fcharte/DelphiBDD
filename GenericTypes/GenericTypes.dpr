7a02465435f044f1aa130e130151dabc4a738870
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-01 10:54:48 +0200

    Método Sort genérico

--- GenericTypes/GenericTypes.dpr
+++ GenericTypes/GenericTypes.dpr
@@ -7,11 +7,41 @@ program GenericTypes;
 uses
   System.SysUtils;
 
+type
+
+TSort<DataType> = class
+ type
+   ArrayType = array of DataType;
+   TFuncComparator = reference to
+       function(l1,l2: DataType): Boolean;
+
+   class procedure Sort(items: ArrayType;
+                        compareFunc: TFuncComparator);
+end;
+
+
+class procedure TSort<DataType>.Sort(items: ArrayType;
+                         compareFunc: TFuncComparator);
+var
+  index, position: Integer;
+  temp: DataType;
+
 begin
-  try
-    { TODO -oUser -cConsole Main : Insert code here }
-  except
-    on E: Exception do
-      Writeln(E.ClassName, ': ', E.Message);
-  end;
+  for index := Low(items)+1 to High(items) do
+    begin
+      temp := items[index];
+      position := index - 1;
+      while ((position >= Low(items)) and
+           compareFunc(temp, items[position])) do
+        begin
+          items[position+1] := items[position];
+          dec(position);
+        end;
+
+       items[position+1] := temp;
+    end;
+end;
+
+begin
+
 end.
