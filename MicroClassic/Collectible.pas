d69b6208ecb8c92384fdb022317d7e0534370230
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-01 09:33:12 +0200

    Declaración constructor

--- MicroClassic/Collectible.pas
+++ MicroClassic/Collectible.pas
@@ -6,22 +6,26 @@ uses
   FMX.Graphics; { Módulo con la definición de TBitmap }
 
 type
-  TLaunchYear    = 1965 .. 1995;
-  TConsState     =    0 .. 100;
+  TLaunchYear = 1965 .. 1995;
+  TConsState = 0 .. 100;
 
-TCollectible = class abstract
+  TCollectible = class abstract
   private
-    Fname        : String;
-    Fyear        : TLaunchYear;
-    Fstate       : TConsState;
-    Fdescription : String;
-    Fpictures    : array of TBitmap;
-    nPictures    : Integer;
+    Fname: String;
+    Fyear: TLaunchYear;
+    Fstate: TConsState;
+    Fdescription: String;
+    Fpictures: array of TBitmap;
+    nPictures: Integer;
     { Métodos de uso interno }
     procedure setName(name: String);
-    function  getPicture(Index: Integer): TBitmap;
+    function getPicture(Index: Integer): TBitmap;
     procedure extendPictures;
-end;
+  public
+    { Sólo hay un constructor.
+      Necesita el nombre del objeto a registrar }
+    constructor Create(name: String);
+  end;
 
 implementation
 
