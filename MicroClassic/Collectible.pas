e509a4a428b574d8cf1700b8e74a150fe28a5d49
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-01 09:27:41 +0200

    Atributos de la clase

--- MicroClassic/Collectible.pas
+++ MicroClassic/Collectible.pas
@@ -1,8 +1,23 @@
 unit Collectible;
 
 interface
+
 uses
   FMX.Graphics; { Módulo con la definición de TBitmap }
+
+type
+  TLaunchYear    = 1965 .. 1995;
+  TConsState     =    0 .. 100;
+
+TCollectible = class abstract
+  private
+    Fname        : String;
+    Fyear        : TLaunchYear;
+    Fstate       : TConsState;
+    Fdescription : String;
+    Fpictures    : array of TBitmap;
+    nPictures    : Integer;
+end;
 
 implementation
 
