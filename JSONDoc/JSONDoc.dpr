030fd2dddf70fa26a8576aa2c72609e2f188aacf
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-15 11:25:28 +0200

    Crear objeto JSON desde cadena

--- JSONDoc/JSONDoc.dpr
+++ JSONDoc/JSONDoc.dpr
@@ -5,8 +5,29 @@ program JSONDoc;
 {$R *.res}
 
 uses
-  System.SysUtils;
+  System.SysUtils, System.JSON;
+
+var
+  cliente: TJSONObject;
 
 begin
+  cliente := TJSONObject.Create;
+  cliente.Parse(BytesOf('{ "Cliente": ' +
+     '{ "Nombre": "Francisco Charte", ' +
+     ' "ClienteHabitual": true, ' +
+     ' "Saldo": 512.08, ' +
+     ' "Direccion": ' +
+     '{ "TipoVia": "Calle", ' +
+     ' "Via": "Bailén", ' +
+     ' "Numero": 56 }, ' +
+     ' "Pedidos": [ { ' +
+     ' "Fecha": "12/6/2014", ' +
+     ' "Importe": 324.65 },{ ' +
+     ' "Fecha": "8/10/2014", ' +
+     ' "Importe": 187.43 }]}}'
+  ), 0);
 
+  WriteLn(cliente.ToString);
+
+  ReadLn;
 end.
