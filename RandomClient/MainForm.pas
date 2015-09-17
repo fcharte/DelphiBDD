bf405137e75b0448a84187462d4255b304df306e
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-17 12:17:44 +0200

    Llamada mediante FireDAC

--- RandomClient/MainForm.pas
+++ RandomClient/MainForm.pas
@@ -4,10 +4,22 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs,
+  FireDAC.Stan.Intf, FireDAC.Stan.Option, FireDAC.Stan.Error, FireDAC.UI.Intf,
+  FireDAC.Phys.Intf, FireDAC.Stan.Def, FireDAC.Stan.Pool, FireDAC.Stan.Async,
+  FireDAC.Phys, FireDAC.Phys.DS, FireDAC.Phys.DSDef, FireDAC.Stan.Param,
+  FireDAC.DatS, FireDAC.DApt.Intf, FireDAC.DApt, FMX.Controls.Presentation,
+  FMX.StdCtrls, Data.DB, FireDAC.Comp.DataSet, FireDAC.Comp.Client,
+  IPPeerClient, FireDAC.Phys.TDBXBase, FireDAC.FMXUI.Wait, FireDAC.Comp.UI;
 
 type
   TfrmMain = class(TForm)
+    RandomserviceConnection: TFDConnection;
+    NextrandProc: TFDStoredProc;
+    Button1: TButton;
+    FDPhysDSDriverLink1: TFDPhysDSDriverLink;
+    FDGUIxWaitCursor1: TFDGUIxWaitCursor;
+    procedure Button1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -20,5 +32,16 @@ var
 implementation
 
 {$R *.fmx}
+
+procedure TfrmMain.Button1Click(Sender: TObject);
+begin
+  with NextrandProc do begin
+    ParamByName('FromN').Value := 25;
+    ParamByName('ToN').Value := 50;
+    ExecProc;
+    ShowMessage(ParamByName('ReturnValue').AsString);
+  end;
+
+end;
 
 end.
