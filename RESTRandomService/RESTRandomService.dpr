16b171e7de47f1f0e184c211e737fd5f999976c7
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-22 12:40:30 +0200

    Proyecto RESTRandomService

---
+++ RESTRandomService/RESTRandomService.dpr
@@ -0,0 +1,22 @@
+program RESTRandomService;
+{$APPTYPE GUI}
+
+{$R *.dres}
+
+uses
+  FMX.Forms,
+  Web.WebReq,
+  IdHTTPWebBrokerBridge,
+  MainForm in 'MainForm.pas' {Form1},
+  ServerMethodsUnit in 'ServerMethodsUnit.pas',
+  WebModuleUnit in 'WebModuleUnit.pas' {WebModule1: TWebModule};
+
+{$R *.res}
+
+begin
+  if WebRequestHandler <> nil then
+    WebRequestHandler.WebModuleClass := WebModuleClass;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ RESTRandomService/ServerMethodsUnit.pas
@@ -0,0 +1,20 @@
+unit ServerMethodsUnit;
+
+interface
+
+uses System.SysUtils, System.Classes, Datasnap.DSServer, Datasnap.DSAuth;
+
+type
+{$METHODINFO ON}
+  TServerMethods1 = class(TComponent)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+{$METHODINFO OFF}
+
+implementation
+
+end.
+
