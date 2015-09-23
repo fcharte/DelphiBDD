ebdc81827b9ee57b1dc8cea58a1e3184165c1695
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-23 18:31:34 +0200

    Configuración parámetros RAnd

--- DelphiRESTClient/MainForm.pas
+++ DelphiRESTClient/MainForm.pas
@@ -5,7 +5,8 @@ interface
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, IPPeerClient,
-  REST.Client, Data.Bind.Components, Data.Bind.ObjectScope;
+  REST.Client, Data.Bind.Components, Data.Bind.ObjectScope, FMX.StdCtrls,
+  FMX.Controls.Presentation;
 
 type
   TfrmMain = class(TForm)
@@ -15,6 +16,9 @@ type
     RESTSeed: TRESTClient;
     RESTSeedRequest: TRESTRequest;
     RESTSeedResponse: TRESTResponse;
+    btnRand: TButton;
+    lblRand: TLabel;
+    procedure btnRandClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -26,6 +30,16 @@ var
 
 implementation
 
+uses System.JSON, REST.Types;
+
 {$R *.fmx}
+
+procedure TfrmMain.btnRandClick(Sender: TObject);
+begin
+//  RESTRandRequest.Params[0].Value := '1';
+ // RESTRandRequest.Params[1].Value := '100';
+  RESTRandRequest.Execute;
+  lblRand.Text := ((RESTRandResponse.JSONValue as TJSONObject).Values['result'] as TJSONArray).Items[0].ToString;
+end;
 
 end.
