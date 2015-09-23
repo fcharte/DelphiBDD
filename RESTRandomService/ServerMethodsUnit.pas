e5d92aace8b02031b282888891e6fafef569a830
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-23 11:22:29 +0200

    Agregado método para cambiar la semilla

--- RESTRandomService/ServerMethodsUnit.pas
+++ RESTRandomService/ServerMethodsUnit.pas
@@ -11,7 +11,8 @@ type
     { Private declarations }
   public
     { Public declarations }
-    function NextRand(FromN: integer; ToN: integer): integer;
+    function Rand(FromN: integer; ToN: integer): integer;
+    function updateRand(seed: integer): integer;
   end;
 {$METHODINFO OFF}
 
@@ -19,9 +20,16 @@ implementation
 
 { TServerMethods1 }
 
-function TRandomMethods.NextRand(FromN, ToN: integer): integer;
+function TRandomMethods.Rand(FromN, ToN: integer): integer;
 begin
   Result :=  Random(ToN-FromN) + FromN;
+end;
+
+
+function TRandomMethods.updateRand(seed: integer): integer;
+begin
+  result := RandSeed;
+  RandSeed := seed;
 end;
 
 end.
