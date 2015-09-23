c6c8f2d2f5f543b5c9418ad4dd41b99816a9d02c
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-23 19:29:53 +0200

    GUI y código asociado

--- DelphiRESTClient/MainForm.pas
+++ DelphiRESTClient/MainForm.pas
@@ -6,19 +6,31 @@ uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
   FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, IPPeerClient,
   REST.Client, Data.Bind.Components, Data.Bind.ObjectScope, FMX.StdCtrls,
-  FMX.Controls.Presentation;
+  FMX.Controls.Presentation, FMX.Edit;
 
 type
   TfrmMain = class(TForm)
     RESTRand: TRESTClient;
     RESTRandRequest: TRESTRequest;
     RESTRandResponse: TRESTResponse;
-    RESTSeed: TRESTClient;
     RESTSeedRequest: TRESTRequest;
     RESTSeedResponse: TRESTResponse;
+    Label1: TLabel;
+    edInferior: TEdit;
+    Label2: TLabel;
+    edSuperior: TEdit;
+    Label6: TLabel;
+    lblResultado: TLabel;
     btnRand: TButton;
-    lblRand: TLabel;
+    GroupBox1: TGroupBox;
+    GroupBox2: TGroupBox;
+    btnSeed: TButton;
+    edSemilla: TEdit;
+    Label3: TLabel;
+    Label5: TLabel;
+    lblResultSeed: TLabel;
     procedure btnRandClick(Sender: TObject);
+    procedure btnSeedClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -36,10 +48,17 @@ uses System.JSON, REST.Types;
 
 procedure TfrmMain.btnRandClick(Sender: TObject);
 begin
-//  RESTRandRequest.Params[0].Value := '1';
- // RESTRandRequest.Params[1].Value := '100';
+  RESTRandRequest.Params[0].Value := edInferior.Text;
+  RESTRandRequest.Params[1].Value := edSuperior.Text;
   RESTRandRequest.Execute;
-  lblRand.Text := ((RESTRandResponse.JSONValue as TJSONObject).Values['result'] as TJSONArray).Items[0].ToString;
+  lblResultado.Text := ((RESTRandResponse.JSONValue as TJSONObject).Values['result'] as TJSONArray).Items[0].ToString;
+end;
+
+procedure TfrmMain.btnSeedClick(Sender: TObject);
+begin
+  RESTSeedRequest.Params[0].Value := edSemilla.Text;
+  RESTSeedRequest.Execute;
+  lblResultSeed.Text := ((RESTSeedResponse.JSONValue as TJSONObject).Values['result'] as TJSONArray).Items[0].ToString;
 end;
 
 end.
