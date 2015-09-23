f0b5144c3c265e8e1f23e663ef26b4a6308bb940
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-23 16:29:34 +0200

    Configurados componentes REST para servicio Rand

--- DelphiRESTClient/MainForm.pas
+++ DelphiRESTClient/MainForm.pas
@@ -4,10 +4,17 @@ interface
 
 uses
   System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
-  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs;
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, IPPeerClient,
+  REST.Client, Data.Bind.Components, Data.Bind.ObjectScope;
 
 type
   TfrmMain = class(TForm)
+    RESTRand: TRESTClient;
+    RESTRandRequest: TRESTRequest;
+    RESTRandResponse: TRESTResponse;
+    RESTSeed: TRESTClient;
+    RESTSeedRequest: TRESTRequest;
+    RESTSeedResponse: TRESTResponse;
   private
     { Private declarations }
   public
