d0b91b3602e6114faa0a85e1942a6653dc6fa403
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-24 11:34:08 +0200

    Proyecto RESTCategories

---
+++ RESTCategories/ServerMethodsUnit.pas
@@ -0,0 +1,26 @@
+unit ServerMethodsUnit;
+
+interface
+
+uses System.SysUtils, System.Classes, System.Json,
+    Datasnap.DSServer, Datasnap.DSAuth, DataSnap.DSProviderDataModuleAdapter;
+
+type
+{$METHODINFO ON}
+  TServerMethods1 = class(TDataModule)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+{$METHODINFO OFF}
+
+implementation
+
+{%CLASSGROUP 'FMX.Controls.TControl'}
+
+{$R *.dfm}
+
+
+end.
+
