aae3fc3279e67a5b68450869efe6d41fbc60146d
Francisco Charte Ojeda <francisco@fcharte.com>
2015-09-25 15:12:23 +0200

    Creación nuevos grupos

--- EMSClient/MainForm.pas
+++ EMSClient/MainForm.pas
@@ -10,7 +10,7 @@ uses
   REST.Backend.Providers, REST.Backend.ServiceComponents, FMX.Layouts,
   FMX.ListBox, Data.Bind.Components, Data.Bind.ObjectScope,
   REST.Backend.BindSource, FMX.Controls.Presentation, FMX.ScrollBox, FMX.Memo,
-  FMX.StdCtrls, REST.Client, REST.Backend.EndPoint;
+  FMX.StdCtrls, REST.Client, REST.Backend.EndPoint, FMX.Edit;
 
 type
   TForm12 = class(TForm)
@@ -25,9 +25,13 @@ type
     BackendUsers1: TBackendUsers;
     BackendStorage1: TBackendStorage;
     BackendAuth1: TBackendAuth;
+    edlGrupo: TEdit;
+    btnGrupo: TButton;
     procedure FormShow(Sender: TObject);
+    procedure btnGrupoClick(Sender: TObject);
   private
-    { Private declarations }
+    procedure RefreshGroups;
+    procedure RefreshUsers;
   public
     { Public declarations }
   end;
@@ -39,28 +43,51 @@ implementation
 
 {$R *.fmx}
 
+procedure TForm12.btnGrupoClick(Sender: TObject);
+var
+  newGroup: TBackendEntityValue;
+begin
+  BackendGroups1.Groups.CreateGroup(edlGrupo.Text, TJSONObject.Create, newGroup);
+  RefreshGroups;
+end;
+
 procedure TForm12.FormShow(Sender: TObject);
 var
   I: Integer;
   EMSClient: TEMSClientAPI;
   userName: String;
 begin
+  RefreshGroups;
+  RefreshUsers;
+
+  //EMSClient := TEMSClientAPI.Create;
+  // Host y puerto del servidor
+  //for userName in EMSClient.RetrieveUsersNames do
+  //  lbUsers.Items.Add(userName);
+end;
+
+procedure TForm12.RefreshGroups;
+var
+  I: Integer;
+begin
+  lbGroups.Clear;
+
   BackendQuery1.BackendService := 'Groups';
   BackendQuery1.Execute;
   with BackendQuery1.JSONResult do
   for I := 0 to Count-1 do
     lbGroups.Items.Add(Items[I].GetValue<String>('groupname'));
+end;
 
+procedure TForm12.RefreshUsers;
+var
+  I: Integer;
+begin
   BackendQuery1.BackendService := 'Users';
   BackendQuery1.Execute;
   with BackendQuery1.JSONResult do
   for I := 0 to Count-1 do
     lbUsers.Items.Add(Items[I].GetValue<String>('username'));
-
-  EMSClient := TEMSClientAPI.Create;
-  // Host y puerto del servidor
-  for userName in EMSClient.RetrieveUsersNames do
-    lbUsers.Items.Add(userName);
 end;
 
 end.
